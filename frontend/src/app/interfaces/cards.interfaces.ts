07611235d3fa6fdfaf8a48e1358c2e15e9d9510d
smarchena <santy832@gmail.com>
2026-05-31 15:10:55 -0500

    added carousel component and updated footer with current year; create Boots, Shirts, and Accessories interfaces

---
+++ frontend/src/app/interfaces/cards.interfaces.ts
@@ -0,0 +1,20 @@
+export interface Boots {
+  title: string,
+  url: string,
+  description: string,
+  precio: number
+}
+
+export interface Shirts {
+  title: string,
+  url: string,
+  description: string,
+  precio: number
+}
+
+export interface Accesories {
+  title: string,
+  url: string,
+  description: string,
+  precio: number
+}
--- frontend/src/app/pages/main/carousel/carousel.ts
+++ frontend/src/app/pages/main/carousel/carousel.ts
@@ -1,9 +1,12 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Component } from '@angular/core';
+import { Slide1 } from './slide-1/slide-1';
+import { Slide2 } from './slide-2/slide-2';
+import { Slide3 } from './slide-3/slide-3';
 
 @Component({
   selector: 'main-carousel',
-  imports: [],
+  imports: [Slide1, Slide2, Slide3],
   templateUrl: './carousel.html',
   styleUrl: './carousel.css',
 })
