eb2335f3c7d648e5e68160ffb4f1054387eae8ad
smarchena <santy832@gmail.com>
2026-06-02 12:07:35 -0500

    header was modified in component and page preview

--- frontend/src/app/shared/header/header.ts
+++ frontend/src/app/shared/header/header.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from './navbar/navbar';
 import { Logo } from './logo/logo';
 import { TopMenu } from './top-menu/top-menu';
-import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-header',
-  imports: [Logo, TopMenu, Navbar],
+  imports: [Navbar, Logo,TopMenu],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
